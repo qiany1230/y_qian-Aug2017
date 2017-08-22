146bb1cf97ebabcc875aab9fc3a4480f4c445db5
Ying Qian <yingq1230@gmail.com>
2017-08-22 09:27:51 -0700

    Second Project -- using Git from Eclipse

---
+++ Project2/src/Project2.java
@@ -0,0 +1,15 @@
+
+// Ying Qian
+// My second java project. It will compare print and println.
+// August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello -- this is project 2!!!");
+
+		System.out.print("hello -- this is project 2!!!");
+		
+	}
+
+}
