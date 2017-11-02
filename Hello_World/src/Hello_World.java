5696d0d7a55de3ea81ab54ec4f25ce9e3a21d81e
Ying Qian <yingq1230@gmail.com>
2017-11-02 08:15:35 -0700

    starting code

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,14 @@
+/**
+ * A message to the world.
+ * 
+ * @Ying Qian
+ * @August 22,2017
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,13 @@
+//Ying Qian
+//8/28/2017
+//Pikachu
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("\tPikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t     (\\__/)");
+		System.out.println("\t\t\t     (o^.^)");
+		System.out.println("\t\t            z(_(\")(\")");
+	}
+
+}
