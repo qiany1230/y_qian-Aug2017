ee2fda18e677fc718cd45dc43ff99479d8d3e347
Ying <yingq1230@gmail.com>
2017-12-10 19:07:59 -0800

    added tests

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -2,7 +2,7 @@
  * September 5, 2017
  * This is the runner class for my Calculate Library methods
  */
-public class DoMath {
+public class DoMath { 
 	
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(5));
