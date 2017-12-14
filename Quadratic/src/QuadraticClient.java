c64a01fda79591870279e8b60beee42769e8f75d
Ying <yingq1230@gmail.com>
2017-12-14 08:17:31 -0800

    finished arrays lab3

--- Quadratic/src/QuadraticClient.java
+++ Quadratic/src/QuadraticClient.java
@@ -7,38 +7,32 @@ import java.util.Scanner;
 
 public class QuadraticClient{
 	public static void main(String[] args) {
+
+		double a;
+		double b;
+		double c;
 		
 		Scanner console = new Scanner(System.in);
-
-		double a = console.nextDouble();
-		double b = console.nextDouble();
-		double c = console.nextDouble();
-		
+	 	String end = console.nextLine();
+		String endit = "quit";
 		System.out.println("Welcome to the Quadratic Describer");
 		System.out.println("Provide values for coefficients a, b, and c");
 		
-			System.out.println("a:" + a);
-			System.out.println("b:" + b);
-			System.out.println("c:" + c);
+		a = console.nextDouble();
+		b = console.nextDouble();
+		c = console.nextDouble();
+		System.out.println("a:" + a);
+		System.out.println("b:" + b);
+		System.out.println("c:" + c);
 		
-			System.out.println(Quadratic.quadrDescriber(a, b, c));
-			System.out.println("");
-			System.out.println("Do you want to keep going? (Type \"quit\" to end");
-
-		console.close();
+		System.out.println(Quadratic.quadrDescriber(a, b, c));
+		System.out.println("");
+		System.out.println("Do you want to keep going? (Type \"quit\" to end");
+			
+		while (end == endit) {
+			console.close();
+		}
+			
+		 	
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
