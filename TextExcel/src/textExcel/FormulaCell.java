a6b07b7c0c9313ade9e090435f351ea0e5893cb0
Ying Qian <yingq1230@gmail.com>
2018-05-01 15:19:25 -0700

    finished part of checkpoint 5

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -3,38 +3,47 @@ package textExcel;
 public class FormulaCell extends RealCell {
 
 	private String[] array;
-	private Spreadsheet grid;
+	private Spreadsheet spreadsheet;
 	
-	public FormulaCell (String input, Spreadsheet grid) {
+	public FormulaCell (String input, Spreadsheet spreadsheet) {
 		
 		super(input);
-		this.grid = grid;
+		this.spreadsheet = spreadsheet;
 		this.array = input.split(" ");
 	}
 
 	@Override
 	public double getDoubleValue() {
 
-		double result = Double.parseDouble(array[1]);
+		double result = getValue(array[1]);
+		double operand = 0;
 		for (int i = 3; i < array.length - 1; i += 2) {
-			//cell reference
-			if (array[1]) {
-			double operand = Double.parseDouble(array[i]);
-			} else() {
-			
+			operand = getValue(array[i]);
 			String operator = array[i-1];
-			if (operator.equals("+")) {
-				result += operand;
-			} else if (operator.equals("-")) {
-				result -= operand;
-			} else if (operator.equals("*")) {
-				result *= operand;
-			} else if (operator.equals("/")) {
-				result /= operand;
+				if (operator.equals("+")) {
+					result += operand;
+				} else if (operator.equals("-")) {
+					result -= operand;
+				} else if (operator.equals("*")) {
+					result *= operand;
+				} else if (operator.equals("/")) {
+					result /= operand;
+				}
 			}
-			}
-		}
 		return result;
 	}
-	
-	public double getValue(String s) {
+	public double getValue (String input) {
+		
+		double operand = 0;
+		if (input.matches(".*[a-zA-Z]+.*")) {
+			SpreadsheetLocation value = new SpreadsheetLocation(input);
+			Cell cell = spreadsheet.getCell(value);
+			if (cell instanceof RealCell) {
+				operand = Double.parseDouble(cell.fullCellText());
+			} else 
+				operand = 0;
+		} else 
+			operand = Double.parseDouble(input);
+		return operand;
+	}
+}
